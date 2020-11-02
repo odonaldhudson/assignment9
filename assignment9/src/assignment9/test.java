b497ec7fb40173e002f6bdf58d06e32bb5d0581f
O'Donald Hudson <odonaldhudson@gmail.com>
2020-11-02 11:16:10 -0600

    assignment 9

---
+++ assignment9/src/assignment9/test.java
@@ -0,0 +1,17 @@
+package assignment9;
+
+public class test {
+	public boolean IsNameQ() {
+		String name = readLine("Enter name: ");
+		return (name == "Q");
+		
+		}
+
+	private String readLine(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
